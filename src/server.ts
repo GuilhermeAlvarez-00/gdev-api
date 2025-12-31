caaa55f7632b40f03a1642778f5fe5853baf83c0
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2025-12-31 12:18:01 -0300

    feat: adding fastify and creating app entry point

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import Fastifiy from "fastify";
+import { userRoutes } from "./routes";
+
+const fastify = Fastifiy({});
+
+fastify.register(userRoutes);
+
+try {
+  fastify.listen({ port: 3000 }).then(() => console.log("Running on port: 3000"));
+} catch (error) {
+  fastify.log.error(error)
+  process.exit(1)
+}
