081beb6c2890ecbef899086ba7e8ebdd274ed060
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2025-12-31 16:24:18 -0300

    feat(user|create-user): new route to create an user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,8 @@
+import { createUserController } from "@modules/user/useCase/create-user";
 import { type FastifyInstance } from "fastify"
 
 export const userRoutes = async (fastify: FastifyInstance, options: any) => {
+  fastify.post("/user/create", createUserController.handler.bind(createUserController));
   fastify.get("/user", (request, reply) => {
     reply.send("Hello world 3");
   });
