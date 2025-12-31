18132ee9a7db7489174f36a1a7dcc71c8bba157f
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2025-12-31 16:25:00 -0300

    feat(utils|return-controller-error): creating util to handle errors in controllers

---
+++ src/utils/return-controller-error.ts
@@ -0,0 +1,15 @@
+import { AppError } from "@abstracts/AppError";
+import type { FastifyReply } from "fastify"
+import { ZodError } from "zod";
+
+export const returnControllerError = (reply: FastifyReply, error: any) => {
+  if (error instanceof AppError) {
+    return reply.status(error.statusCode!).send({ message: error?.message ?? "" })
+  }
+
+  if (error instanceof ZodError) {
+    return reply.status(400).send(error.issues);
+  }
+
+  return reply.status(400).send(error);
+}
