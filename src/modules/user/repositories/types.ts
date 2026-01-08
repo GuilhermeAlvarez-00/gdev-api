df3b0708dd6ac032ff3ea058709a07cae102018e
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2026-01-07 22:27:48 -0300

    feat(repository|user): adding new method findAll

--- src/modules/user/repositories/types.ts
+++ src/modules/user/repositories/types.ts
@@ -3,3 +3,8 @@ import type { TUserEntity } from "@entities/user/user.entity";
 export type TUser = TUserEntity & {
   id: string;
 };
+
+export type TFindAllUsersParams = {
+  name?: string | undefined;
+  email?: string | undefined;
+};
